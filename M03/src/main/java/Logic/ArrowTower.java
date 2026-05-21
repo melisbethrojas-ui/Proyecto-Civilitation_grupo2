b0621cdbe419658fee348cbeb32408a8e57be217
Anna121104 <119697202+AnnSan00@users.noreply.github.com>
2026-05-21 15:21:29 +0200

    .

--- M03/src/main/java/Logic/ArrowTower.java
+++ M03/src/main/java/Logic/ArrowTower.java
@@ -4,7 +4,7 @@ package Logic;
 public class ArrowTower extends DefenseUnit {
 
     // Constructor jugador
-    // que calcula la tecnología directamente en el 'super'
+    // que calcula la tecnología directamente en el 'super'(DefenseUnit) para no tener que crear atributos adicionales en esta clase
     public ArrowTower(int technologyArmorLevel, int technologyAttackLevel) {
         super(
             ARMOR_ARROWTOWER + (ARMOR_ARROWTOWER * technologyArmorLevel * PLUS_ARMOR_ARROWTOWER_BY_TECHNOLOGY) / 100,
